6d6d4db997f2ecbe6a89459ea5f6b68263a66890
saby537 <saby537@gmail.com>
2020-08-05 02:10:41 +0530

    Added SASS and Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Homepage from "./homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Homepage />
     </div>
   );
 }
