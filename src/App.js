b9cdadab8ab63cc8c23906f6e6cbc6ec5c39af20
saby537 <saby537@gmail.com>
2020-08-06 02:11:20 +0530

    Updated Menu-Item to use withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/Homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
