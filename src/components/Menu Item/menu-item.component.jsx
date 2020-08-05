7d4ac09ceb2fb54962ef9a56207323aedead857b
saby537 <saby537@gmail.com>
2020-08-06 01:22:42 +0530

    Broken Homepage into smaller components, Finished Styling Menu-Item

---
+++ src/components/Menu Item/menu-item.component.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    />
+    <div className="content">
+      <div className="title">{title.toUpperCase()}</div>
+      <div className="subtitle">SHOP NOW</div>
+    </div>
+  </div>
+);
+
+export default MenuItem;
