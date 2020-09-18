9df952fe0d3ed332d6e9eac014489ea9008ed134
saby537 <saby537@gmail.com>
2020-09-19 02:08:07 +0530

    Added Mobile Responsiveness

--- client/src/components/header/header.styled.component.jsx
+++ client/src/components/header/header.styled.component.jsx
@@ -2,29 +2,41 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const HeaderContainer = styled.div`
-  height: 70px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 5px;
+	height: 70px;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 5px;
+
+	@media screen and (max-width: 800px) {
+		margin-bottom: 20px;
+	}
 `;
 
 export const LogoContainer = styled(Link)`
-  height: 100%;
-  width: 70px;
-  padding: 10px;
+	height: 100%;
+	width: 70px;
+	padding: 10px;
 `;
 
 export const OptionsContainer = styled.div`
-  width: 50%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  font-weight: bold;
+	width: 50%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+	font-weight: bold;
+
+	@media screen and (max-width: 800px) {
+		width: 80%;
+	}
 `;
 
 export const OptionLink = styled(Link)`
-  padding: 10px 15px;
-  cursor: pointer;
+	padding: 10px 15px;
+	cursor: pointer;
+
+	@media screen and (max-width: 800px) {
+		padding: 10px 10px;
+	}
 `;
--- client/src/components/sign-up/sign-up.component.jsx
+++ client/src/components/sign-up/sign-up.component.jsx
@@ -6,70 +6,72 @@ import { signUpStart } from "../../redux/user/user.actions";
 import "./sign-up.styles.scss";
 
 const SignUp = ({ signUp }) => {
-  const [userCredentials, setUserCredentials] = useState({
-    displayName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-  const { displayName, email, password, confirmPassword } = userCredentials;
+	const [userCredentials, setUserCredentials] = useState({
+		displayName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const { displayName, email, password, confirmPassword } = userCredentials;
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if (password !== confirmPassword) {
-      alert("Passwords donot match");
-      return;
-    }
-    signUp({ email, password, displayName });
-  };
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setUserCredentials({ ...userCredentials, [name]: value });
-  };
-  return (
-    <div className="sign-up">
-      <h2 title="title">I donot have an account</h2>
-      <span>Sign up with your email and password</span>
-      <form onSubmit={handleSubmit}>
-        <FormInput
-          type="text"
-          name="displayName"
-          value={displayName}
-          onChange={handleChange}
-          label="Display Name"
-          required
-        />
-        <FormInput
-          type="email"
-          name="email"
-          value={email}
-          onChange={handleChange}
-          label="Email"
-          required
-        />
-        <FormInput
-          type="password"
-          name="password"
-          value={password}
-          onChange={handleChange}
-          label="Password"
-          required
-        />
-        <FormInput
-          type="password"
-          name="confirmPassword"
-          value={confirmPassword}
-          onChange={handleChange}
-          label="Confirm Password"
-          required
-        />
-        <CustomButton type="submit">SIGN UP</CustomButton>
-      </form>
-    </div>
-  );
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		if (password !== confirmPassword) {
+			alert("Passwords donot match");
+			return;
+		}
+		signUp({ email, password, displayName });
+	};
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setUserCredentials({ ...userCredentials, [name]: value });
+	};
+	return (
+		<div className="sign-up">
+			<h2 title="title">I donot have an account</h2>
+			<span>Sign up with your email and password</span>
+			<form onSubmit={handleSubmit}>
+				<FormInput
+					type="text"
+					name="displayName"
+					value={displayName}
+					onChange={handleChange}
+					label="Display Name"
+					required
+				/>
+				<FormInput
+					type="email"
+					name="email"
+					value={email}
+					onChange={handleChange}
+					label="Email"
+					required
+				/>
+				<FormInput
+					type="password"
+					name="password"
+					value={password}
+					onChange={handleChange}
+					label="Password"
+					required
+				/>
+				<FormInput
+					type="password"
+					name="confirmPassword"
+					value={confirmPassword}
+					onChange={handleChange}
+					label="Confirm Password"
+					required
+				/>
+				<div className="signup-btn">
+					<CustomButton type="submit">SIGN UP</CustomButton>
+				</div>
+			</form>
+		</div>
+	);
 };
 const mapDispatchToProps = (dispatch) => ({
-  signUp: (userCredentials) => dispatch(signUpStart(userCredentials)),
+	signUp: (userCredentials) => dispatch(signUpStart(userCredentials)),
 });
 
 export default connect(null, mapDispatchToProps)(SignUp);
