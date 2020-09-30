7433391a18883b7e0d86cae2f76d6642ff663fd5
saby537 <saby537@gmail.com>
2020-10-01 01:33:02 +0530

    Added Service Worker, updated manifest and HTTPS support

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
+const enforce = require("express-sslify");
+
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -12,9 +14,11 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
-
+app.get("/service-worker.js", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
 app.post("/payment", (req, res) => {
 	const body = {
 		source: req.body.token.id,
